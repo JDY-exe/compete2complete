2c15dac217680bc52b680a54baf04d86c807f1e6
John Yuan <yuan411@purdue.edu>
2024-10-26 16:18:18 -0400

    init router

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,12 @@
+import './Login.css'
+function Login() {
+    return(
+        <div className='bg'>
+            Hello world
+            <div className=''></div>
+        </div>
+    )
+
+}
+
+export default Login
