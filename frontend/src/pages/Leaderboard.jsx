3b5644e07740856d918d1c8138165dcbb590aba0
John Yuan <yuan411@purdue.edu>
2024-10-26 20:55:57 -0400

    updated lb css

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,6 +1,6 @@
 import './Leaderboard.css'
 function Leaderboard() {
-    return(
+    return (
 
         <div class="lb-container">
             <div className="lb-header">
@@ -16,13 +16,13 @@ function Leaderboard() {
                     <div className="player-image"></div>
                     <div className="player-card-content">
                         <div className="player-card-header">
-                        <span className="player-card-name">Marin Kitagawa</span>
-                        <span className="player-card-points">102 pts</span>
+                            <span className="player-card-name">Marin Kitagawa</span>
+                            <span className="player-card-points">102 pts</span>
                         </div>
                         <div className="player-progress-bar">
-                        <div 
-                            className="player-progress-fill" style={ {width: "50%"} }
-                        ></div>
+                            <div
+                                className="player-progress-fill" style={{ width: "50%" }}
+                            ></div>
                         </div>
                     </div>
                 </div>
@@ -31,29 +31,55 @@ function Leaderboard() {
                     <div className="player-image"></div>
                     <div className="player-card-content">
                         <div className="player-card-header">
-                        <span className="player-card-name">Gojo Satoru</span>
-                        <span className="player-card-points">10 pts</span>
+                            <span className="player-card-name">Gojo Satoru</span>
+                            <span className="player-card-points">10 pts</span>
                         </div>
                         <div className="player-progress-bar">
-                        <div 
-                            className="player-progress-fill" style={ {width: "5%"} }
-                        ></div>
+                            <div
+                                className="player-progress-fill" style={{ width: "5%" }}
+                            ></div>
                         </div>
                     </div>
                 </div>
             </div>
 
-                
+
 
 
             <div className="lb-taskboard">
                 <div className="lb-subheader">Taskboard</div>
 
                 {/* insert individual tasks */}
+
+
+
+                <div className="lb-task-card">
+                    <div className="lb-task-header">
+                        <h1 className="lb-task-title">DSA HW 5</h1>
+                        <button className="lb-task-delete-button">X</button>
+                    </div>
+
+                    <p className="lb-task-description">Probably the worst wording on a homework ever.</p>
+
+                    <div className="lb-task-footer">
+                        <input
+                            type="text"
+                            className="lb-points-input"
+                            value={120}
+                            aria-label="Points"
+                        />
+                        <button className="lb-mark-complete">
+                            Mark complete
+                        </button>
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     )
 
-} 
+}
 
 export default Leaderboard
